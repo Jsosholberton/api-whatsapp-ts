b8925dc39d1e5e9403cdb236f850a542175c4dca
Johnatan Ortiz <6403@holbertonstudents.com>
2024-04-22 18:29:03 -0500

    UPDATE: added cors options

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,23 @@ import cors from "cors"
 import routes from "./infrastructure/router"
 const port = process.env.PORT || 3001
 const app = express()
-app.use(cors())
+
+const whitelist = [process.env.FRONTEND_URL];
+
+const corsOptions = {
+  origin: function (origin: any, callback: any) {
+    if (whitelist.includes(origin)) {
+      // Allow the request to proceed if the origin is in the whitelist
+      callback(null, true);
+    } else {
+      // Reject the request with an error if the origin is not in the whitelist
+      callback(new Error("Error of Cors"));
+    }
+  },
+};
+
+app.use(cors(corsOptions))
+
 app.use(express.json())
 app.use(express.static('tmp'))
 app.use(`/`,routes)
