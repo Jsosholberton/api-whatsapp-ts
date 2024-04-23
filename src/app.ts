b8f486d3c48af2a9da290a9200872a2606201b04
Johnatan Ortiz <6403@holbertonstudents.com>
2024-04-23 10:00:16 -0500

    Added https

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import routes from "./infrastructure/router"
+import https from "https"
+import fs from "fs"
+
 const port = process.env.PORT || 3001
 const app = express()
 
@@ -25,4 +28,15 @@ app.use(express.json())
 app.use(express.static('tmp'))
 app.use(`/`,routes)
 
-app.listen(port, () => console.log(`Ready...${port}`))
+const privateKeyPath = "/etc/letsencrypt/live/johnatanortiz.tech/privkey.pem";
+const certificatePath = "/etc/letsencrypt/live/johnatanortiz.tech/fullchain.pem";
+
+const privateKey = fs.readFileSync(privateKeyPath, 'utf8');
+const certificate = fs.readFileSync(certificatePath, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+  console.log(`Ready...${port}`);
+});
